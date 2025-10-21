6419da9e13c02ae514b54653097a7c725a09cba8
root <nicherry2009@gmail.com>
2025-10-21 15:41:01 +0800

    update run_grpo

--- run_grpo.sh
+++ run_grpo.sh
@@ -12,16 +12,18 @@ train_batch_size=256
 micro_batch_size=8
 use_std_normalization=True
 #gradient_accumulation_steps = 128 # microbatch size is 2, will fit on H100 gpu_memory_utilization: float = 0.85
-#loss_type="reinforce_with_baseline"
+loss_type="reinforce_with_baseline"
 # grpo_learning_rate: 调最优lr
 # grpo_baselines: 比较比较有基线 (reinforce_with_baseline) 和无基线 (no_baseline) 的策略梯度方法的性能差异
-sub_experiment="grpo_learning_rate"
+
 
 # 训练 grpo 模型
 # 1. 基于作业给的参数, 比较不同learning_rate的效果
 #learning_rate=e-5
 #learning_rate=3e-5
+sub_experiment="grpo_learning_rate"
 learning_rate=2e-5
+loss_type="reinforce_with_baseline"
 #echo "python cs336_alignment/my_async_train_grpo.py --sub_experiment $sub_experiment --learning_rate $learning_rate --loss_type $loss_type > grpo_${sub_experiment}_${learning_rate}_${current_time}.log 2>&1"
 #python cs336_alignment/my_train_grpo.py --sub_experiment $sub_experiment --learning_rate $learning_rate --loss_type $loss_type
 #python cs336_alignment/my_train_grpo.py --sub_experiment $sub_experiment --learning_rate $learning_rate --loss_type $loss_type > grpo_${sub_experiment}_${learning_rate}_${current_time}.log 2>&1
@@ -34,8 +36,16 @@ echo "python cs336_alignment/my_async_train_grpo.py --sub_experiment $sub_experi
 python cs336_alignment/my_async_train_grpo.py --sub_experiment $sub_experiment --learning_rate $learning_rate --loss_type $loss_type > grpo_${sub_experiment}_${learning_rate}_${current_time}.log 2>&1
 
 
-# 3. use_std_normalization=False
-sub_experiment="effect_of_std_normalization"
+# 3. grpo_length_normalization
+sub_experiment="grpo_length_normalization"
+loss_type="reinforce_with_baseline"
+#masked_normalize/masked_mean
+masked_mean_or_normalize="masked_normalize"
+#Compare normalization with masked_mean and masked_normalize
+python cs336_alignment/my_async_train_grpo.py --sub_experiment $sub_experiment --learning_rate $learning_rate --loss_type $loss_type --masked_mean_or_normalize $masked_mean_or_normalize > grpo_${sub_experiment}_${learning_rate}_${current_time}.log 2>&1
+
+# 4. use_std_normalization=False
+sub_experiment="grpo_group_standard_deviation"
 use_std_normalization=False
 python cs336_alignment/my_async_train_grpo.py --sub_experiment $sub_experiment --learning_rate $learning_rate --loss_type $loss_type --use_std_normalization $use_std_normalization > grpo_${sub_experiment}_${learning_rate}_nostd_${current_time}.log 2>&1
 
